c1f9bac8e0e06a9c4f1ac8f2bc51de8c53d3cdbd
Dan Leaver <dleaver@gmail.com>
2021-03-02 15:56:32 -0700

    radio boxes and readCamel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+const radioButtons = [
+  "trivia",
+  "math",
+  "year",
+  "date",
+]
+
+const readCamel = (str) => str.charAt(0).toUpperCase() + str.slice(1).replace(/([a-z\d])([A-Z])/g, '$1' + " " + '$2')
 
 function App() {
+  const [formValues, setFormValues] = useState({
+    number: "",
+    category: "trivia"
+  })
+
+  const fetchData = () => {
+    axios.get(`http://numbersapi.com/${formValues.number}/${formValues.category}`)
+      .then(res => console.log(res.data))
+      .catch(console.log)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchData()
+  }
+
+  const handleChange = (e) => {
+    setFormValues({...formValues, [e.target.name]: e.target.value})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input name="number" onChange={handleChange} value={formValues.number} />
+        <div>
+          {radioButtons.map((item, idx) => (
+            <div key={idx}>
+              <label>{readCamel(item)}</label>
+              <input onChange={handleChange} type="radio" name="category" value={item} defaultChecked={!idx}/>
+            </div>
+          ))}
+        </div>
+        <button type="submit"> get it done </button>
+      </form>
     </div>
   );
 }
